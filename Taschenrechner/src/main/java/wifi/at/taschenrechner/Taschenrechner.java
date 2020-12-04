39e96606165b7fac4a0f2961a18710b0c37ffa85
AsliAsliyuece <75502949+AsliAsliyuece@users.noreply.github.com>
2020-12-04 21:39:28 +0100

    Add files via upload

---
+++ Taschenrechner/src/main/java/wifi/at/taschenrechner/Taschenrechner.java
@@ -0,0 +1,26 @@
+/*
+ * by: Asli Asliyüce
+ */
+package wifi.at.taschenrechner;
+
+/**
+ *
+ * @author aa_design
+ */
+public class Taschenrechner {
+    public static void main(String[] args) {
+        
+    }
+    double a = 45;
+    double b = 5;
+    
+    public static int add(int a, int b){
+    return a+b;
+    }
+    
+    public static int sub(int a, int b){
+    return a-b;
+    }
+    
+    
+}
